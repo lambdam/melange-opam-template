6578dcf1083436cf8d9d158fca47e6f68e4d7bf9
Damien Ragoucy <dam@ramdam.io>
2024-03-03 23:56:57 +0100

    WIP

--- src/Hello.ml
+++ src/Hello.ml
@@ -1,1 +1,3 @@
-let () = Js.log ("Hello " ^ World.name ^ "!!")
+let delay ms =
+  Js.Promise.make (fun ~resolve ~reject:_ ->
+    ignore (Js.Global.setTimeout ~f:(fun () -> (resolve true [@u])) ms))
